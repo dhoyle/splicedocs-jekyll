112b06aa86ddff8a08b3deb4dfab325eb6494f3f
Gary Hillerson <ghillerson@splicemachine.com>
2017-11-19 20:04:26 -0800

    updates to configuration files, sitemap generator, and build script to fix problems with multiple, independent version.

--- servesplice.sh
+++ servesplice.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-read -p "Enter the version to build (doc, docstest, docsdev) [docstest]: " version
+read -p "Enter the version to build (doc, docstest, docsdev)      [docstest]: " version
 version=${version:-docstest}
-read -p "Enter the branch to build (2.7.0, 2.6.1, 2.6.0, 2.5) [2.6.1]: " branch
+read -p "Enter the branch to build (2.7.0, 2.6.1, 2.6.0, 2.5)     [2.6.1]: " branch
 branch=${branch:-2.6.1}
 jekyll serve --config=_config.yml,_config_$version.yml,_config_$branch.yml
