9ebce57b7084956d77076b5e57ef5386040cc0ea
Gary Hillerson <ghillerson@splicemachine.com>
2018-02-16 14:02:41 -0800

    Initial (very ugly) version of devguide.pdf work

--- servepdf-devguide.sh
+++ servepdf-devguide.sh
@@ -4,11 +4,10 @@ echo 'Killing all Jekyll instances'
 kill -9 $(ps aux | grep '[j]ekyll' | awk '{print $2}')
 clear
 
-echo "Building PDF-friendly HTML site for Mydoc ...";
-bundle exec jekyll serve --detach --config _config.yml,pdfconfigs/config_mydoc_pdf.yml;
+echo "Building PDF-friendly HTML site for Developer's Guide ...";
+jekyll serve --detach --config _config.yml,_config_2.7.yml,_config_doc.yml,pdfconfigs/config_devguide_pdf.yml;
 echo "done";
 
 echo "Building the PDF ...";
-prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/mydoc.pdf;
+prince --javascript --input-list=_site/pdfconfigs/prince-list.txt -o pdf/devguide.pdf;
 echo "done";
-
